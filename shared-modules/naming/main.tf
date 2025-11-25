7bc0fa5d371e0afcee175ab3e8a1cc510e3e0e5d
Josh Dinndorf <joshd@emergentsoftware.net>
2025-11-25 10:30:04 -0600

    updating naming module

--- shared-modules/naming/main.tf
+++ shared-modules/naming/main.tf
@@ -4,10 +4,10 @@
 # =============================================================================
 
 module "naming" {
-  source = "git::https://github.com/Azure/terraform-azurerm-naming.git?ref=55e932f8edf91c50e6acf0bd62042766b2d2a120"
-  suffix = var.suffix
-  prefix = var.prefix
-  unique-seed = var.unique_seed
-  unique-length = var.unique_length
+  source                 = "git::https://github.com/Azure/terraform-azurerm-naming.git?ref=55e932f8edf91c50e6acf0bd62042766b2d2a120"
+  suffix                 = var.suffix
+  prefix                 = var.prefix
+  unique-seed            = var.unique_seed
+  unique-length          = var.unique_length
   unique-include-numbers = var.unique_include_numbers
 }
